e65ea9d57dfe3211a0c3032fc25b41310e3a14d5
Joe Johnson <joseph.johnson27001@gmail.com>
2025-07-07 10:06:14 +0700

    Mood Tracker styles

--- src/features/MoodTracker/MoodTracker.jsx
+++ src/features/MoodTracker/MoodTracker.jsx
@@ -1,9 +1,10 @@
 import MoodInput from "./MoodInput";
 import MoodDisplay from "./MoodDisplay";
+import "./MoodTracker.css";
 
 export default function MoodTracker() {
   return (
-    <section>
+    <section className="mood-tracker">
       <h2>Mood Tracker</h2>
       <MoodInput />
       <MoodDisplay />
