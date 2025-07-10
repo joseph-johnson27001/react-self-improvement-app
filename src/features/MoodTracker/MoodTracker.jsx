414254c93c8a6607db57c3179f0fb1d8100ff1f9
Joe Johnson <joseph.johnson27001@gmail.com>
2025-07-10 12:17:31 +0700

    Updated layout and styles for site

--- src/features/MoodTracker/MoodTracker.jsx
+++ src/features/MoodTracker/MoodTracker.jsx
@@ -5,7 +5,6 @@ import "./MoodTracker.css";
 export default function MoodTracker() {
   return (
     <section className="mood-tracker">
-      <h2>Mood Tracker</h2>
       <MoodInput />
       <MoodDisplay />
     </section>
